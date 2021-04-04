75a1af42fd7128ce69ccb53844a7171d8dbbf984
Aanxhel <ang.core92@yahoo.com>
2021-04-04 00:26:41 -0600

    agrego archivo 02

---
+++ 01-EjeIntroVar/js/01-let.js
@@ -0,0 +1,10 @@
+/*
+cracion de comando let
+let: nos permite cambiar los valores relacionados con ellos
+*/
+
+let peso ;
+let masa ;
+
+console.log(peso);
+console.log(masa);
---
+++ 01-EjeIntroVar/js/02-var.js
@@ -0,0 +1,4 @@
+var meses = 12;
+var cantidadDeAlumnos = 30
+
+console.log(meses)

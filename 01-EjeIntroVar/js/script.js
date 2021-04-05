f443168fd5801e818c3d62e33e7f7046d0c6a6ef
Aanxhel <ang.core92@yahoo.com>
2021-04-05 00:22:44 -0500

    reformat

--- 01-EjeIntroVar/js/script.js
+++ 01-EjeIntroVar/js/script.js
@@ -1,15 +1,12 @@
-function evalua(numeroInicial,numeroFinal) {
+function evalua(numeroInicial, numeroFinal) {
     let demo1 = 11;
     let demo2 = "saludo";
 
-    if(numeroInicial < demo1){
-     return ('el numero es menor')
-    }else{
-        return ( demo2 +' el numero es mayor'+ numeroFinal)
+    if (numeroInicial < demo1) {
+        return ('el numero es menor')
+    } else {
+        return (demo2 + ' el numero es mayor' + numeroFinal)
     }
-
-
-    
 }
 
-console.log(evalua(12,"mensaje de salida"))
+console.log(evalua(12, "mensaje de salida"))

737e6d76108d923048eb40d8bd9b7b11afdd312b
Aanxhel <ang.core92@yahoo.com>
2021-04-05 00:21:35 -0500

    eje de muestra

---
+++ 01-EjeIntroVar/js/script.js
@@ -0,0 +1,15 @@
+function evalua(numeroInicial,numeroFinal) {
+    let demo1 = 11;
+    let demo2 = "saludo";
+
+    if(numeroInicial < demo1){
+     return ('el numero es menor')
+    }else{
+        return ( demo2 +' el numero es mayor'+ numeroFinal)
+    }
+
+
+    
+}
+
+console.log(evalua(12,"mensaje de salida"))
